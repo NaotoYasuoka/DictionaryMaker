14429c725bdac3c4fba6c3e25fb52a8fa98e4e57
NaotoYasuoka <58966640+NaotoYasuoka@users.noreply.github.com>
2020-09-12 22:59:08 +0900

    :tada: 追加

--- DictionryMaker.py
+++ DictionryMaker.py
@@ -3,21 +3,14 @@ import os, csv
 def divideText(path):
 
   # readlineで1行だけ読み込み
-  with open(path, "r", encoding="utf_8") as f:
+  with open(path, "r", encoding="Shift_JIS") as f:
     line = f.readline()
+    # print(line)
     while line:
-      try:
-        line = line.decode("utf-8")
-      except UnicodeDecodeError:
-        # utf-8でないバイト列が含まれる行はスキップする
-        line = f.readline()
-        continue
-        # ...
-      # 何か行いたい処理
       str = line.lstrip("■")
       if((str[0] <= "A" | str[0] <= "L") & (str[0] <= "a" | str[0] <= "l")):
         print(line)
-      # ...
+    #   # ...
     #   line = f.readline()
     # str = line.lstrip("■")
     # if((str[0] <= "A" | str[0] <= "L") & (str[0] <= "a" | str[0] <= "l")):
@@ -25,7 +18,7 @@ def divideText(path):
   # f.close()
 
 def main():
-  original_data_path = "../../Dictionary/EIJIRO-1448.TXT"
+  original_data_path = "./dictionary/EIJIROU.txt"
   divideText(original_data_path)
 
 if __name__ == "__main__":
