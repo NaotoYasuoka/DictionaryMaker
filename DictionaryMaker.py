f07c32d41d71609ca6ad2b91061ed7da719b0629
NaotoYasuoka <58966640+NaotoYasuoka@users.noreply.github.com>
2020-09-13 15:09:52 +0900

    修正

--- DictionaryMaker.py
+++ DictionaryMaker.py
@@ -9,6 +9,7 @@ def makeWord(str):
   for w in words:
     i=0
     if((i==1) & (w == "")):
+      print(w)
       word = words[0]
     i=i+1
   return word
@@ -25,26 +26,27 @@ def wordNumber(str):
 
   # if((word=makeWord(word_pos)) != None):
   #   makeMean(means)
-
-  return makeWord(word_pos)
+  a = makeWord(word_pos)
+  return a
 
 def divideText(path, write_path):
   # readlineで1行だけ読み込み
-  # with open(path, "r", encoding="Shift_JIS") as f:
-  with open(path, "r", encoding="cp932") as f:
+  with open(path, "r", encoding="utf-8") as f:
+  #with open(path, "r", encoding="cp932") as f:
     with open(write_path, mode="a", encoding="utf-8") as w:
   #with open(path, "r", encoding="utf-8") as f:
       line = f.readline()
       while line:
         Str = line.rstrip()   # 改行の削除
         str = Str.lstrip("■") # "■"の削除
-        if(((str[0]>="A") | (str[0]>="a")) & ((str[0]<="Z") | (str[0]<="z"))):
-          # if(wordNumber(str) != None):
-          w.write(str+"\n")
+        if(((str[0]>="A")  & (str[0]<="Z")) | ((str[0]>="a") & (str[0]<="z"))):
+          if(wordNumber(str) != None):
+            w.write(str+"\n")
         line = f.readline()
 
 def main():
-  original_data_path = "../../dictionary/EIJIRO-1448.TXT"
+  #original_data_path = "../../dictionary/EIJIRO-1448.TXT"
+  original_data_path = "./dictionary/EIJIROU.txt"
   write_path = "../../dictionary/text.txt"
   divideText(original_data_path, write_path)
 
