b246546bcd6842311fc5259a75e6a34a7d428b6b
NaotoYasuoka <yasupon.out@gmail.com>
2020-09-13 11:50:04 +0900

    :sparkles: 名前の取得

---
+++ DictionaryMaker.py
@@ -0,0 +1,44 @@
+import os, csv
+import sys
+
+def divideText(path):
+
+  # readlineで1行だけ読み込み
+  #with open(path, "r", encoding="Shift_JIS") as f:
+  with open(path, "r", encoding="utf-8") as f:
+    line = f.readline()
+    while line:
+      Str = line.rstrip()   # 改行の削除
+      str = Str.lstrip("■") # "■"の削除
+      if(((str[0]>="A") | (str[0]>="a")) & ((str[0]<="Z") | (str[0]<="z"))):
+        #print(str)
+        splitStr = str.split(":")
+        words = splitStr[0]
+        means = splitStr[1]
+
+        #word = words.replace("\{", "")
+        word = words.rstrip(' ')
+         #word = re.sub(r'\{[^)]*\}', '', words)
+        print(word)
+        # word = words.split(" ")
+        # for w in word: 
+        #   if(w[0] != " "){
+            
+        #   }
+
+        # words = words.replace("{","(")
+        # # words = words.replace("")
+        # # words="cccc  (dddddee}  fff"
+        # word = words.rstrip("(")
+        print(word)
+        
+      #else:
+        #print("Error: configuration failed", file=sys.stderr)
+      line = f.readline()
+
+def main():
+  original_data_path = "./dictionary/EIJIROU.txt"
+  divideText(original_data_path)
+
+if __name__ == "__main__":
+    main()
