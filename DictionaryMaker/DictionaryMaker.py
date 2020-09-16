c0f43cbac6bd5a228f29cdf4ee58ede70a15e750
NaotoYasuoka <58966640+NaotoYasuoka@users.noreply.github.com>
2020-09-16 12:04:47 +0900

    :sparkles: 細分化した辞書の追加

--- DictionaryMaker/DictionaryMaker.py
+++ DictionaryMaker/DictionaryMaker.py
@@ -87,8 +87,8 @@ def divideText(str):
   obj={}
   splitStr = str.split(":")
   str1 = splitStr[0].rstrip("{")
-  obj["word"] = str1
-  obj["means"] = splitStr[1]
+  #obj["word"] = str1
+  #obj["means"] = splitStr[1]
   return obj
 
 def read_write_File(read_paths, write_path, writeType):
@@ -97,25 +97,46 @@ def read_write_File(read_paths, write_path, writeType):
     with open(r_path, mode="r", encoding="cp932") as r_f:
       line = r_f.readline()
       while line:
-        wordInfo = divideText(line)
-        saveFile(w_f, wordInfo, writeType)
+        #wordInfo = divideText(line)
+        saveFile(write_path, line, writeType)
         line = r_f.readline()
 
-def saveFile(file, wordInfo, writeType):
-  if((wordInfo["word"] != "") & (wordInfo["means"] != "")):
+def saveFile(write_path, wordInfo, writeType):
+  #if((wordInfo["word"] != "") & (wordInfo["means"] != "")):
     if("csv" == writeType):
       with open(write_path, "a", encoding="utf-8",newline='') as w_f:
-        writer = csv.writer(file)
+        writer = csv.writer(w_f)
         writer.writerow([wordInfo["word"],wordInfo["means"]])
     elif("txt" == writeType):
-      with open(write_path, "a", encoding="utf-8",newline='') as w_f:
-        file.write(wordInfo["word"]+":"+wordInfo["means"])
+      if(((wordInfo[1]>="A")  & (wordInfo[1]<="M")) | ((wordInfo[1]>="a") & (wordInfo[1]<="m"))):
+        with open(write_path, "a", encoding="utf-8",newline='') as w_f:
+          w_f.write(wordInfo)
     elif("test" == writeType):
       print(wordInfo["word"]+":"+wordInfo["means"])
     else:
       print("The only read types are 'csv' and 'txt'.",file=sys.stderr)
 
+
+def divideFile():
+  # 辞書の細分化
+  with open("../../../dictionary/EIJIRO-1448.TXT", mode="r", encoding="cp932") as r_f:
+    with open("./dictionary/EIJIRO_A-M.txt", mode="a", encoding="utf-8") as w_f_a:
+      with open("./dictionary/EIJIRO_N-Z.txt", mode="a", encoding="utf-8") as w_f_n:
+        line = r_f.readline()
+        i = 0
+        while line:
+          str = line
+          if(((str[1]>="A")  & (str[1]<="M")) | ((str[1]>="a") & (str[1]<="m"))):
+            w_f_a.write(str)
+          if(((str[1]>="N")  & (str[1]<="Z")) | ((str[1]>="n") & (str[1]<="z"))):
+            w_f_n.write(str)
+          i+=1
+          line = r_f.readline()
+        print("word num:", i)
+
+
 def main():
+  divideFile()
   read_paths = ["../../../dictionary/EIJIRO-1448.TXT"]
   # read_path = "../../../dictionary/EIJIRO_.txt"
   #read_paths = ["./dictionary/EIJIRO_A-M.txt", "./dictionary/EIJIRO_N-Z.txt"]
@@ -123,7 +144,7 @@ def main():
   #read_path = "../../../dictionary/EIJIRO_.txt"
   # write_path = "./dictionary/EIJIRO_treatment.csv"
   write_path = "./dictionary/EIJIRO_A-M.txt"
-  read_write_File(read_paths, write_path, "test")
+  # read_write_File(read_paths, write_path, "txt")
 
 if __name__ == "__main__":
     main()
